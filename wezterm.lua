e35d17b4b6c51b74e8e44fafcf9bf818e5c558c1
coulc <542587642@qq.com>
2026-04-25 13:29:00 +0800

    Add wezterm config

---
+++ wezterm.lua
@@ -0,0 +1,23 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.automatically_reload_config = true
+config.enable_tab_bar = false
+config.window_close_confirmation = "NeverPrompt"
+config.window_decorations = "RESIZE"
+config.color_scheme = "default"
+config.font_size = 18
+config.initial_cols = 120
+config.initial_rows = 35
+config.window_padding = {
+  left = 3,
+  right = 3,
+  top = 0,
+  bottom = 0,
+}
+config.window_background_opacity = 0.65
+config.macos_window_background_blur = 20
+
+return config
+
